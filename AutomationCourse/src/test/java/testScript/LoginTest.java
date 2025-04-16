07b8a2bde7a062da69ec9e358ae3ab408df52c2e
Ranil <Ranil@DESKTOP-V42TCMT>
2025-04-16 20:24:19 +0530

    commit and push

---
+++ AutomationCourse/src/test/java/testScript/LoginTest.java
@@ -0,0 +1,49 @@
+package testScript;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import seleniumBasics.TestngBase;
+
+public class LoginTest extends TestngBase {
+
+	@Test
+	public void verifyUserLoginwithValidCredentials() {
+		WebElement usernamefield = driver.findElement(By.id("user-name"));
+		usernamefield.sendKeys("standard_user");
+		WebElement passwordfield = driver.findElement(By.id("password"));
+		passwordfield.sendKeys("secret_sauce");
+		WebElement loginbutton = driver.findElement(By.id("login-button"));
+		loginbutton.click();
+	}
+
+	@Test
+	public void verifyUserLOginwithInvalidUsername() {
+		WebElement usernamefield = driver.findElement(By.id("user-name"));
+		usernamefield.sendKeys("standard_user1");
+		WebElement passwordfield = driver.findElement(By.id("password"));
+		passwordfield.sendKeys("secret_sauce");
+		WebElement loginbutton = driver.findElement(By.id("login-button"));
+		loginbutton.click();
+	}
+
+	@Test
+	public void verifyUserLOginwithInvalidPassword() {
+		WebElement usernamefield = driver.findElement(By.id("user-name"));
+		usernamefield.sendKeys("standard_user");
+		WebElement passwordfield = driver.findElement(By.id("password"));
+		passwordfield.sendKeys("secret_sauce1");
+		WebElement loginbutton = driver.findElement(By.id("login-button"));
+		loginbutton.click();
+	}
+	@Test
+	public void verifyUserLoginwithInvalidCredentials() {
+		WebElement usernamefield = driver.findElement(By.id("user-name"));
+		usernamefield.sendKeys("standard_user1");
+		WebElement passwordfield = driver.findElement(By.id("password"));
+		passwordfield.sendKeys("secret_sauce1");
+		WebElement loginbutton = driver.findElement(By.id("login-button"));
+		loginbutton.click();
+	}
+}
